a386420e831f0cbba6f943b611217e4d4ec78bf8
bhuvanm2002 <bhuvanm8@gmail.com>
2016-07-22 16:26:48 +0530

    QUIZ (COUNTRY-CAPITALS)
    THIS IS A QUIZ OF COUNTRY CAPITAL CITY
    THIS HAS 26 QUESTIONS

---
+++ QUIZ.cpp
@@ -0,0 +1,518 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int qone;
+	cout << " WELCOME TO THE QUIZ , \n THIS QUIZ WAS CREATED BY BHUVAN MATHAD \n THIS QUIZ CONTAINS  26 QUESTION ABOUT COUNTRY CAPITALS " << endl;
+	cout << " INSTRUCTIONS : \n TYPE 1 FOR OPTION a , TYPE 2 FOR OPTION b , TYPE 3 FOR OPTION c , TYPE 4 FOR OPTION d \n BEST OF LUCK !!! " << endl;
+	cout << " Q1. WHAT IS THE CAPITAL CITY OF INDIA : \n a. new delhi \n b. los angeles \n c. hong kong \n d. jersulaem " << endl;
+	cin >> qone;
+
+	if (qone == 1)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qone >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF INDIA IS NEW DELHI :( !!! " << endl;
+	}
+
+
+	int qtwo;
+	cout << " Q2. WHAT IS THE CAPITAL CITY OF UNITED STATES OF AMERICA : \n a. beijing \n b. texas \n c. washington d.c \n d. sydney " << endl;
+	cin >> qtwo;
+
+	if (qtwo == 3)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qtwo >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF UNITED STATES OF AMERICA IS WASHINGTON :( !!! " << endl;
+	}
+
+
+	int qthree;
+	cout << " Q3. WHAT IS THE CAPITAL CITY OF AUSTRAILIA : \n a. mumbai \n b. canberra \n c. sydney \n d. hospet " << endl;
+	cin >> qthree;
+
+	if (qthree == 2)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qthree >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF AUSTRAILIA IS CANBERRA :( !!! " << endl;
+	}
+
+
+	int qfour;
+	cout << " Q4. WHAT IS THE CAPITAL CITY OF ISRAEL : \n a. tampa \n b. banglore \n c. alaska \n d. jersulaem " << endl;
+	cin >> qfour;
+
+	if (qfour == 4)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qfour >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF ISRAEL IS jersulaem :( !!! " << endl;
+	}
+
+	int qfive;
+	cout << " Q5. WHAT IS THE CAPITAL CITY OF CANADA : \n a. ottowa \n b. montreal \n c. toronto \n d. pittsburg " << endl;
+	cin >> qfive;
+
+	if (qfive == 1)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qfive >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF CANADA IS OTTOWA :( !!! " << endl;
+	}
+
+	int qsix;
+	cout << " Q6. WHAT IS THE CAPITAL CITY OF china : \n a. tampa \n b. beijing \n c. alaska \n d. moscow " << endl;
+	cin >> qsix;
+
+	if (qsix == 2)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qsix >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF CHINA IS BEIJING :( !!! " << endl;
+	}
+
+	int qseven;
+	cout << " Q7. WHAT IS THE CAPITAL CITY OF UNITED KINGDOM : \n a. edinburg \n b. manchester \n c. scotland \n d. london " << endl;
+	cin >> qseven;
+
+	if (qseven == 4)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qseven >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF UK IS LONDON :( !!! " << endl;
+	}
+
+	int qeight;
+	cout << " Q8. WHAT IS THE CAPITAL CITY OF IRELAND : \n a. seoul \n b. pyangang \n c. dublin \n d. taipai " << endl;
+	cin >> qeight;
+
+	if (qeight == 3)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qeight >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF IRELAND IS DUBLIN :( !!! " << endl;
+	}
+
+	int qnine;
+	cout << " Q9. WHAT IS THE CAPITAL CITY OF JAPAN : \n a. hokido \n b. tokyo \n c. shillong \n d. kentucky " << endl;
+	cin >> qnine;
+
+	if (qnine == 2)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qnine >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF JAPAN IS TOKYO :( !!! " << endl;
+	}
+
+	int qten;
+	cout << " Q10. WHAT IS THE CAPITAL CITY OF SOUTH KOREA : \n a. seoul \n b. pyangang \n c. shangi \n d. hiroshima " << endl;
+	cin >> qten;
+
+	if (qten == 1)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qten >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF SOUTH KOREA IS SEOUL :( !!! " << endl;
+	}
+
+	int qeleven;
+	cout << " Q11. WHAT IS THE CAPITAL CITY OF NORTH KOREA : \n a. colombo \n b. pyangang \n c.  \n d. taipai " << endl;
+	cin >> qeleven;
+
+	if (qeleven == 2)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qeleven >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF NORTH KOREA IS PYANGANG :( !!! " << endl;
+	}
+
+	int qtwelve;
+	cout << " Q12. WHAT IS THE CAPITAL CITY OF ARGENTINA : \n a. madrid \n b. paris \n c. lisbon \n d. buenos aireas " << endl;
+	cin >> qtwelve;
+
+	if (qtwelve == 4)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qtwelve >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF ARGENTINA IS BUENOS AIREAS :( !!! " << endl;
+	}
+
+	int qthirteen;
+	cout << " Q13. WHAT IS THE CAPITAL CITY OF GERMANY : \n a. berlin \n b. czech \n c. paris \n d. sau paula " << endl;
+	cin >> qthirteen;
+
+	if (qthirteen == 1)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qthirteen >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF GERMANY IS BERLIN :( !!! " << endl;
+	}
+
+	int qfourteen;
+	cout << " Q14. WHAT IS THE CAPITAL CITY OF PORTUGAL ? : \n a. colorado \n b. florida \n c. tiki \n d. lisbon " << endl;
+	cin >> qfourteen;
+
+	if (qfourteen == 4)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qfourteen >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF PORTUGAL IS LISBON :( !!! " << endl;
+	}
+
+	int qfifteen;
+	cout << " Q15. WHAT IS THE CAPITAL CITY OF SPAIN : \n a. utah \n b. paragauy \n c. madrid \n d. taipai " << endl;
+	cin >> qfifteen;
+
+	if (qfifteen == 3)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qfifteen >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF SPAIN IS MADRID :( !!! " << endl;
+	}
+
+	int qsixteen;
+	cout << " Q16. WHAT IS THE CAPITAL CITY OF FRANCE : \n a. miami \n b. francis \n c. pope \n d. paris " << endl;
+	cin >> qsixteen;
+
+	if (qsixteen == 4)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qsixteen >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF FRANCE IS PARIS :( !!! " << endl;
+	}
+
+	int qeighteen;
+	cout << " Q17. WHAT IS THE CAPITAL CITY OF TAIWAN : \n a. taipai \n b. nagasaki \n c. minamoto \n d. kyragryz " << endl;
+	cin >> qeighteen;
+
+	if (qeighteen == 1)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qeighteen >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF TAIWAN IS TAIPAI :( !!! " << endl;
+	}
+
+	int qninteen;
+	cout << " Q18. WHAT IS THE CAPITAL CITY OF BELGIUM : \n a. stockholm \n b. oslo \n c. brussels \n d. czech " << endl;
+	cin >> qninteen;
+
+	if (qninteen == 3)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qninteen >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF BELGIUM IS BRUSSELS :( !!! " << endl;
+	}
+
+	int qtwenty;
+	cout << " Q19. WHAT IS THE CAPITAL CITY OF SWEDEN : \n a. stockholm \n b. greenland \n c. iceland \n d. arctic " << endl;
+	cin >> qtwenty;
+
+	if (qtwenty == 1)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qtwenty >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF SWEDEN IS STOCKHOLM :( !!! " << endl;
+	}
+
+	int qto;
+	cout << " Q20. WHAT IS THE CAPITAL CITY OF NORWAY : \n a. zurich \n b. oslo \n c. ginkhaki \n d. kyoto " << endl;
+	cin >> qto;
+
+	if (qto == 2)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qto >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF NORWAY IS OSLO :( !!! " << endl;
+	}
+
+	int qtt;
+	cout << " Q21. WHAT IS THE CAPITAL CITY OF SWITZERLAND : \n a. bern \n b. zurich \n c. cambridge \n d. oxford " << endl;
+	cin >> qtt;
+
+	if (qtt == 1)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qtt >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF SWITZERLAND IS BERN :( !!! " << endl;
+	}
+
+	int qtthree;
+	cout << " Q22. WHAT IS THE CAPITAL CITY OF RUSSIA : \n a. nigeria \n b. congo \n c. moscow \n d. mexico city " << endl;
+	cin >> qtthree;
+
+	if (qtthree == 3)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qtthree >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF RUSSIA IS MOSCOW :( !!! " << endl;
+	}
+
+	int qtf;
+	cout << " Q23. WHAT IS THE CAPITAL CITY OF SOUTH AFRICA : \n a. algeria \n b. somalia \n c. kenya \n d. johannesbug " << endl;
+	cin >> qtf;
+
+	if (qtf == 4)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qtf >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF SOUTH AFRICA IS JOHANNESBURG :( !!! " << endl;
+	}
+
+	int qtfive;
+	cout << " Q24. WHAT IS THE CAPITAL CITY OF EGYPT : \n a. anicient \n b. noida \n c. cairo \n d. niagra " << endl;
+	cin >> qtfive;
+
+	if (qtfive == 3)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qtfive >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF EGYPT IS CAIRO :( !!! " << endl;
+	}
+
+	int qts;
+	cout << " Q25. WHAT IS THE CAPITAL CITY OF NEW ZEALAND : \n a. wellington \n b. pyangang \n c. washington \n d. amsterdam " << endl;
+	cin >> qts;
+
+	if (qts == 1)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qts >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF NEW ZEALAND IS WELLINGTON :( !!! " << endl;
+	}
+
+	int qtz;
+	cout << " Q26. WHAT IS THE CAPITAL CITY OF UAE ( UNITED ARAB EMIRATES) : \n a. dubai \n b. abu dhabi \n c. dublin \n d. new york " << endl;
+	cin >> qtz;
+
+	if (qtz == 2)
+	{
+		cout << " YOU ARE RIGHT :) !!! " << endl;
+	}
+
+	else if (qtz >= 5)
+	{
+		cout << " READ THE INSTRUCTIONS IT CLEARLY SAYS YOU NEED TO TYPE 1 TO 4 DEPENDING ON THE QUESTION :{ " << endl;
+	}
+
+	else
+	{
+		cout << " YOU ARE WRONG THE CAPITAL CITY OF UAE IS ABU DHABI :( !!! " << endl;
+	}
+
+	cout << " SORRY NOW WE ARE RAN OUT OF QUESTION :{ , BUT WE KNOW THAT YOU LIKED THIS GAME  "  << endl;
+	cout << " THANK YOU FOR BEING AND PLAYING WITH US \n HOPE YOU WILL COME AGAIN AND PLAY THIS GAME !!!!  " << endl; 
+	cout << " THIS GAME WAS CREATED BY BHUVAN MATHAD " << endl;
+
+	return 0;
+
+	// THIS GAME WAS CREATED BY BHUVAN MATHAD
+
+	/*
+	THIS GAME WAS CREATED IN 20.06.2016 
+	AT 9.41 PM
+	*/
+
+}
